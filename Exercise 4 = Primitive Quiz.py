c7e6d5d7855c36948ab77fdeac08e74443a8bb6b
saqibromaan2 <saqibromaan2@gmail.com>
2025-10-29 16:52:04 +0400

    Delete Exercise 4 = Primitive Quiz.py

--- Exercise 4 = Primitive Quiz.py
+++
@@ -1,8 +0,0 @@
-# Ask the user the question
-answer = input("What is the capital of France? ")
-
-# Check if the answer is correct
-if answer.lower() == "paris":
-    print("Correct! The capital of France is Paris.")
-else:
-    print("Wrong answer. The correct answer is Paris.")
